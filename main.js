fddae244e862b6b474fc6592fe8cc8e8d9b17191
André Filho <AndreFilhoSan@gmail.com>
2022-03-17 22:14:02 -0300

    Projeto Urna Eletronica

---
+++ main.js
@@ -0,0 +1,31 @@
+const condidatos = (nome, numero, partido, cargo) => {
+    return {
+        nome: 'nome',
+        numero: 'numero',
+        partido: 'partido',
+        cargo: 'cargo',
+    }
+}
+
+const partidos = {
+    partido1: {
+        numero: 1,
+        nome: 'Partido 1'
+    },
+    partido2: {
+        numero: 2,
+        nome: 'Partido 2'
+    },
+    partido3: {
+        numero: 3,
+        nome: 'Partido 3'
+    },
+    partido3: {
+        numero: 4,
+        nome: 'Partido 4'
+    },
+    partido5: {
+        numero: 5,
+        nome: 'Partido 5'
+    },
+}
