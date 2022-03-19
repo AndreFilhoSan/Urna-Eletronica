581f81c7e980da1ddba705c8a9f6109ce879fc8a
André Filho <AndreFilhoSan@gmail.com>
2022-03-18 22:02:02 -0300

    Atualizações

---
+++ ideias.js
@@ -0,0 +1,32 @@
+let numUsuario = ''
+let campoNumero = ''
+
+
+
+const localTecla = document.querySelectorAll('button')
+localTecla.addEventListener('click',numeroTeclado)
+
+const numeroTeclado = () => {
+if (localTecla === document.querySelector('#num1')){
+    console.log(localTecla)
+} else {
+    console.log(localTecla)
+}
+}
+
+const numTeclado = () => {
+  
+}
+
+// Variável 'num' para cada número do teclado.
+num1 = document.querySelector('#num1')
+num2 = document.querySelector('#num2')
+num3 = document.querySelector('#num3')
+num4 = document.querySelector('#num4')
+num5 = document.querySelector('#num5')
+num6 = document.querySelector('#num6')
+num7 = document.querySelector('#num7')
+num8 = document.querySelector('#num8')
+num9 = document.querySelector('#num9')
+num0 = document.querySelector('#num0')
+
